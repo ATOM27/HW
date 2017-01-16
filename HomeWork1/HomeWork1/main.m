ffa6cfeab71eb335fd59de52ff87c83459e45ae9
ATOM27 <gek2395@mail.ru>
2017-01-16 19:26:47 +0200

    HomeWork 1

---
+++ HomeWork1/HomeWork1/main.m
@@ -0,0 +1,51 @@
+//
+//  main.m
+//  HomeWork1
+//
+//  Created by Eugene Mekhedov on 16.01.17.
+//  Copyright © 2017 Eugene Mekhedov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSArray* arrayWithSongs = [[NSArray alloc] initWithObjects: @"Papaoutai",
+                                                                    @"Summertime Sadness",
+                                                                    @"Diamond",
+                                                                    @"Stole the show",
+                                                                    @"Ta Fete", nil];
+        
+        NSArray* arrayWithArtists = [[NSArray alloc] initWithObjects:@"Rihanna",
+                                                                     @"Kygo",
+                                                                     @"Stromae",
+                                                                     @"Lana Del Ray", nil];
+        
+  
+        NSArray* arrayWithSongsAndArtists = @[@"Papaoutai - Stromae",
+                                            @"Diamond - Rihanna",
+                                            @"Stole the show - Kygo",
+                                            @"Summertime Sadness - Lana Del Ray",
+                                            @"Ta Fete - Stromae"];
+
+        for (NSString* currentSong in arrayWithSongs){
+            
+            for (NSString* songAndArtist in arrayWithSongsAndArtists){
+                
+                if ([songAndArtist containsString:currentSong]){
+                    NSArray* arrayWithComponents = [songAndArtist componentsSeparatedByString:@" - "];
+                    
+                    NSString* object = [arrayWithComponents objectAtIndex:1];
+                    NSInteger index = [arrayWithArtists indexOfObject:object];
+                    
+                    NSString* currentArtist = [arrayWithArtists objectAtIndex:index];
+                    
+                    NSLog(@"The song \"%@\" by %@", currentSong, currentArtist);
+                    break;
+                }
+            }
+        }
+}
+    return 0;
+}
