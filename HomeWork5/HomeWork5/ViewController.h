63eb43bdb150bde38ae3e07b52f2dd44f108e9a7
ATOM27 <gek2395@mail.ru>
2017-01-23 23:33:43 +0200

    HW5

---
+++ HomeWork5/HomeWork5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeWork5
+//
+//  Created by Eugene Mekhedov on 23.01.17.
+//  Copyright © 2017 Eugene Mekhedov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
