6c12bce9e47becc384f9ccf70854f1a0e42f4311
Eugene Mekhedov <gek2395@mail.ru>
2017-02-06 14:27:33 +0200

    Lecture 13

---
+++ Party_Maker_Mekhedov/Party_Maker_Mekhedov/EMLoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  EMLoginViewController.h
+//  Party_Maker_Mekhedov
+//
+//  Created by intern on 2/6/17.
+//  Copyright © 2017 Eugene Mekhedov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMLoginViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UIView *loginView;
+@property (strong, nonatomic) IBOutlet UITextField *loginTextField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordTextField;
+@end
---
+++ Party_Maker_Mekhedov/Party_Maker_Mekhedov/EMLoginViewController.m
@@ -0,0 +1,66 @@
+//
+//  EMLoginViewController.m
+//  Party_Maker_Mekhedov
+//
+//  Created by intern on 2/6/17.
+//  Copyright © 2017 Eugene Mekhedov. All rights reserved.
+//
+
+#import "EMLoginViewController.h"
+
+@interface EMLoginViewController ()
+
+@end
+
+NS_ENUM(NSInteger, EMTextField){
+    EMTextFieldLogin,
+    EMTextFieldPassword
+};
+
+@implementation EMLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    UIView* paddingLogginView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 0)];
+    self.loginTextField.leftView = paddingLogginView;
+    self.loginTextField.leftViewMode = UITextFieldViewModeAlways;
+    
+    UIView* paddingPasswordView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 10, 0)];
+
+    self.passwordTextField.leftView = paddingPasswordView;
+    self.passwordTextField.leftViewMode = UITextFieldViewModeAlways;
+    
+    self.loginTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Login"
+                                                                                     attributes:@{NSForegroundColorAttributeName:[UIColor colorWithRed:76.f/255.f green:82.f/255.f blue:92.f/255.f alpha:1.f],
+                                                                                                  }
+                                                      ];
+    self.passwordTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Password"
+                                                                                attributes:@{NSForegroundColorAttributeName:[UIColor colorWithRed:76.f/255.f green:82.f/255.f blue:92.f/255.f alpha:1.f],
+                                                                                             }
+                                                 ];
+    
+
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    if(textField.tag == EMTextFieldLogin){
+        [self.passwordTextField becomeFirstResponder];
+    }else{
+        [textField resignFirstResponder];
+    }
+    
+    return YES;
+}
+
+
+@end
